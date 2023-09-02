c0c1eceff0c52dea51fb0496d9ec51056354e131
stefanopriolo <stefanojpriolo@gmail.com>
2023-09-02 17:42:13 +0200

    Hero

--- src/components/TheHero.vue
+++ src/components/TheHero.vue
@@ -3,33 +3,40 @@
 
 <template>
     <section class="pt-5">
-        <div class="container d-flex text-white p-5">
+        <div class="container d-flex text-white pt-5">
             <div class="row pt-5">
-                <h1 class="display-1">Unlock Your Online Growth Potential</h1>
-                <h4>Online marketing to secure customer retention, leads, and sales. We focus on the bigger picture.</h4>
-                <button class="btn btn-outline-success my-2 my-sm-0 btn-lg" type="submit">Free Quote</button>
-            </div>
-            <div class="row">
-                <div class="card" style="width: 20rem;">
-                    <img class="card-img-top" src="../assets/images/marketing-consultant-expert.jpg" alt="Card image cap">
-                    <div class="card-body">
-                        <h5 class="card-title">Richard Madsen</h5>
-                        <p class="card-text">Marketing Consultant Expert</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                        <a class="navbar-brand ps-5" href="#"><i class="fa-solid fa-phone pe-3" style="color: #f86011;"></i>(555) 802-1234</a>
-                    <form>
-                        <div class="row">
-                            <div class="col">
-                                <input type="text" class="form-control" placeholder="First name">
-                            </div>
-                            <div class="col">
-                                <input type="text" class="form-control" placeholder="Last name">
-                            </div>
-                        </div>
-                    </form>
+                <div class="col-7 pt-5">
+                    <h1 class="display-1"><b>Unlock Your Online Growth Potential</b></h1>
+                <h2>Online marketing to secure customer retention, leads, and sales. We focus on the bigger picture.</h2>
+                <button class="btn btn-outline-success my-sm-0" type="submit">Free Quote</button>
+                </div>
+                <div class="col-5 pt-5">
+                    <div class="card card-xl position-absolute p-3">
+                    <div class="card-img text-center">
+                        <img class="card-img-top" src="../assets/images/marketing-consultant-expert.jpg" alt="Card image cap">
                     </div>
+                    
+                    <div class="card-body text-center">
+                        <h5 class="card-title" style="font-size: 1.5rem;">Richard Madsen</h5>
+                        <p class="card-text pt-2" style="color:#848684; font-size: 1.2rem;">Marketing Consultant Expert</p>
+                        <i class="fa-solid fa-phone pe-3 orange"></i>
+                        <span class="orange"><b>(555) 802-1234</b></span>
+                        <form>
+                            <div class="row p-1 pt-4">
+                                <input type="text" class="form-control mb-2 p-3" placeholder="Your Name*">
+                                <input type="email" class="form-control mb-2 p-3" placeholder="Your Email*">
+                                <input type="tel" class="form-control mb-2 p-3" placeholder="Your Phone Number*">
+                                <button type="button" class="btn orangebg text-white mb-2 p-3" data-toggle="button" aria-pressed="false" autocomplete="off">
+                                    Get A Callback
+                                </button>
+                                <span class="smalltext" style="color:#848684">By submitting my data I agree to be contacted</span>
+                            </div>
+                    </form>
+                </div>
+                </div>
                 </div>
             </div>
+          
         </div>
         
     </section>
@@ -39,12 +46,30 @@
 section {
     background-image: url(../assets/images/marketing-intro.jpg);
     background-size: cover;
+    height: 720px;
 
 
     img {
-        width: 50px;
+        width: 70px;
         border-radius: 50%;
     }
+
+    .orange {
+        color: #f86011;
+    }
+    
+    .orangebg {
+        background-color: #f86011;
+    }
+
+    .smalltext {
+        font-size: 0.7rem;
+    }
+
+    .card-xl {
+    width: 400px; 
+    height: auto; 
+}
 }
 
 
